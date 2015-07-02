9c393451df8d6f4cb9aed26e26ae19de0fa67e55
Sidiq Permana <permana.sidiq@gmail.com>
2015-07-02 15:22:21 +0700

    Initial commit

---
+++ app/src/main/java/com/nbs/app/sampleintent/Sub2Activity.java
@@ -0,0 +1,47 @@
+package com.nbs.app.sampleintent;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class Sub2Activity extends ActionBarActivity {
+
+    public static String KEY_DATA = "data";
+    private String receivedData = null;
+    private TextView txtData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub2);
+
+        txtData = (TextView)findViewById(R.id.txt_data);
+        receivedData = getIntent().getStringExtra(KEY_DATA);
+        txtData.setText(receivedData);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sub2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
